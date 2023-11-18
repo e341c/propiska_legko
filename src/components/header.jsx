38099a90d12ae75aafc1958c3c1d232040371187
e341c <tabyserik@gmail.com>
2023-11-18 16:29:39 +0600

    navbar toggle button color changed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
         <Navbar expand="lg" className="bg position-fixed top-0 w-100 z-3 shadow">
             <Container>
                 <NavbarBrand className="text-light">Прописка <strong>Легко</strong> </NavbarBrand>
-                <NavbarToggle aria-controls="basic-navbar-nav"  />
+                <NavbarToggle aria-controls="basic-navbar-nav" className="white-filter" />
                 <NavbarCollapse id="basic-navbar-nav">
                     <Nav className="ms-auto align-items-center">
                         <NavLink href={"tel:+77017156363"} className="text-light fs-5">+7 (701) 715-63-63</NavLink>
